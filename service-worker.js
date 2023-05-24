00a0a52994a0cb87b89af360fad1b43935188585
Miguel Rivas <mrivaslezama@gmail.com>
2023-05-23 20:23:01 -0400

    adding build

---
+++ service-worker.js
@@ -0,0 +1,203 @@
+const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), u = [
+  a + "/_app/immutable/entry/app.d182aeb5.js",
+  a + "/_app/immutable/assets/0.2fa32c42.css",
+  a + "/_app/immutable/nodes/0.7a16d0b6.js",
+  a + "/_app/immutable/nodes/1.428d9b35.js",
+  a + "/_app/immutable/assets/404.9de340c4.webp",
+  a + "/_app/immutable/assets/2.aef64abf.css",
+  a + "/_app/immutable/nodes/2.14907bf5.js",
+  a + "/_app/immutable/nodes/3.170ddec9.js",
+  a + "/_app/immutable/assets/4.7893dccc.css",
+  a + "/_app/immutable/nodes/4.bfe61d7c.js",
+  a + "/_app/immutable/nodes/5.a9432666.js",
+  a + "/_app/immutable/nodes/6.1388c050.js",
+  a + "/_app/immutable/assets/404.096a76f1.avif",
+  a + "/_app/immutable/chunks/code_copy.242746cb.js",
+  a + "/_app/immutable/chunks/i18n-svelte.e29bc5df.js",
+  a + "/_app/immutable/assets/avatar.c4879091.png",
+  a + "/_app/immutable/assets/avatar.9b7b25c7.avif",
+  a + "/_app/immutable/assets/avatar.d9375b86.webp",
+  a + "/_app/immutable/assets/avatar.a241e23c.png",
+  a + "/_app/immutable/assets/avatar.f73e53d4.png",
+  a + "/_app/immutable/assets/avatar.2f35c48e.png",
+  a + "/_app/immutable/assets/avatar.cba3f947.png",
+  a + "/_app/immutable/assets/qwer.77ae3ceb.webp",
+  a + "/_app/immutable/chunks/image_banner.0655567c.js",
+  a + "/_app/immutable/assets/avatar.82b068da.avif",
+  a + "/_app/immutable/assets/default_og_card.18695b75.avif",
+  a + "/_app/immutable/assets/default_og_card.5034a8c4.avif",
+  a + "/_app/immutable/assets/default_og_card.df8c7476.webp",
+  a + "/_app/immutable/assets/default_og_card.3dcbfd44.avif",
+  a + "/_app/immutable/assets/mobile-preview.d5a12dea.avif",
+  a + "/_app/immutable/assets/mobile-preview.ce99b627.avif",
+  a + "/_app/immutable/assets/mobile-preview.67d10d03.png",
+  a + "/_app/immutable/assets/mobile-preview.81312f37.avif",
+  a + "/_app/immutable/assets/mobile-preview.091d9a91.avif",
+  a + "/_app/immutable/assets/mobile-preview.33f5d44f.avif",
+  a + "/_app/immutable/assets/mobile-preview.775bf4d7.webp",
+  a + "/_app/immutable/assets/mobile-preview.4c0544e3.avif",
+  a + "/_app/immutable/assets/preview.61f6c960.avif",
+  a + "/_app/immutable/assets/preview.a5dcbe25.avif",
+  a + "/_app/immutable/assets/preview.d1b275c8.png",
+  a + "/_app/immutable/assets/preview.4b0d4713.avif",
+  a + "/_app/immutable/assets/preview.912e099f.avif",
+  a + "/_app/immutable/assets/preview.caeec78e.avif",
+  a + "/_app/immutable/assets/preview.08386647.webp",
+  a + "/_app/immutable/assets/preview.75842b34.avif",
+  a + "/_app/immutable/assets/qwer.6dac4acc.avif",
+  a + "/_app/immutable/assets/qwer.2fd8e116.avif",
+  a + "/_app/immutable/assets/qwer.1aa68c61.avif",
+  a + "/_app/immutable/assets/github-mark.7a0dd11e.svg",
+  a + "/_app/immutable/assets/cover.bd92074e.avif",
+  a + "/_app/immutable/assets/cover.63cc43ef.avif",
+  a + "/_app/immutable/assets/cover.3dcf3d95.jpg",
+  a + "/_app/immutable/assets/cover.245e453a.avif",
+  a + "/_app/immutable/assets/example1.e74f1512.jpg",
+  a + "/_app/immutable/assets/example1.941acbe2.avif",
+  a + "/_app/immutable/assets/cover.befe241c.avif",
+  a + "/_app/immutable/assets/cover.2098f6b0.avif",
+  a + "/_app/immutable/assets/cover.0a503f79.jpg",
+  a + "/_app/immutable/assets/cover.4a04002f.avif",
+  a + "/_app/immutable/assets/reach.c7188d48.avif",
+  a + "/_app/immutable/assets/reach.fc8a39a9.avif",
+  a + "/_app/immutable/assets/reach.926a2713.jpg",
+  a + "/_app/immutable/assets/reach.85740fa5.avif",
+  a + "/_app/immutable/assets/wait.051e8f09.avif",
+  a + "/_app/immutable/assets/wait.15cd711b.avif",
+  a + "/_app/immutable/assets/wait.8861bec1.jpg",
+  a + "/_app/immutable/assets/wait.dff50a19.avif",
+  a + "/_app/immutable/assets/cover.18b3d8e6.avif",
+  a + "/_app/immutable/assets/cover.dcfa05c8.avif",
+  a + "/_app/immutable/assets/cover.a89320e0.jpg",
+  a + "/_app/immutable/assets/cover.95f1adce.avif",
+  a + "/_app/immutable/chunks/index.72eb2937.js",
+  a + "/_app/immutable/chunks/post.152f8fc1.js",
+  a + "/_app/immutable/assets/post.3c17f144.css",
+  a + "/_app/immutable/assets/KaTeX_AMS-Regular.0cdd387c.woff2",
+  a + "/_app/immutable/assets/KaTeX_AMS-Regular.30da91e8.woff",
+  a + "/_app/immutable/assets/KaTeX_AMS-Regular.68534840.ttf",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Bold.de7701e4.woff2",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Bold.1ae6bd74.woff",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Bold.07d8e303.ttf",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Regular.5d53e70a.woff2",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Regular.3398dd02.woff",
+  a + "/_app/immutable/assets/KaTeX_Caligraphic-Regular.ed0b7437.ttf",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Bold.74444efd.woff2",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Bold.9be7ceb8.woff",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Bold.9163df9c.ttf",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Regular.51814d27.woff2",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Regular.5e28753b.woff",
+  a + "/_app/immutable/assets/KaTeX_Fraktur-Regular.1e6f9579.ttf",
+  a + "/_app/immutable/assets/KaTeX_Main-Bold.0f60d1b8.woff2",
+  a + "/_app/immutable/assets/KaTeX_Main-Bold.c76c5d69.woff",
+  a + "/_app/immutable/assets/KaTeX_Main-Bold.138ac28d.ttf",
+  a + "/_app/immutable/assets/KaTeX_Main-BoldItalic.99cd42a3.woff2",
+  a + "/_app/immutable/assets/KaTeX_Main-BoldItalic.a6f7ec0d.woff",
+  a + "/_app/immutable/assets/KaTeX_Main-BoldItalic.70ee1f64.ttf",
+  a + "/_app/immutable/assets/KaTeX_Main-Italic.97479ca6.woff2",
+  a + "/_app/immutable/assets/KaTeX_Main-Italic.f1d6ef86.woff",
+  a + "/_app/immutable/assets/KaTeX_Main-Italic.0d85ae7c.ttf",
+  a + "/_app/immutable/assets/KaTeX_Main-Regular.c2342cd8.woff2",
+  a + "/_app/immutable/assets/KaTeX_Main-Regular.c6368d87.woff",
+  a + "/_app/immutable/assets/KaTeX_Main-Regular.d0332f52.ttf",
+  a + "/_app/immutable/assets/KaTeX_Math-BoldItalic.dc47344d.woff2",
+  a + "/_app/immutable/assets/KaTeX_Math-BoldItalic.850c0af5.woff",
+  a + "/_app/immutable/assets/KaTeX_Math-BoldItalic.f9377ab0.ttf",
+  a + "/_app/immutable/assets/KaTeX_Math-Italic.7af58c5e.woff2",
+  a + "/_app/immutable/assets/KaTeX_Math-Italic.8a8d2445.woff",
+  a + "/_app/immutable/assets/KaTeX_Math-Italic.08ce98e5.ttf",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Bold.e99ae511.woff2",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Bold.ece03cfd.woff",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Bold.1ece03f7.ttf",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Italic.00b26ac8.woff2",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Italic.91ee6750.woff",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Italic.3931dd81.ttf",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Regular.68e8c73e.woff2",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Regular.11e4dc8a.woff",
+  a + "/_app/immutable/assets/KaTeX_SansSerif-Regular.f36ea897.ttf",
+  a + "/_app/immutable/assets/KaTeX_Script-Regular.036d4e95.woff2",
+  a + "/_app/immutable/assets/KaTeX_Script-Regular.d96cdf2b.woff",
+  a + "/_app/immutable/assets/KaTeX_Script-Regular.1c67f068.ttf",
+  a + "/_app/immutable/assets/KaTeX_Size1-Regular.6b47c401.woff2",
+  a + "/_app/immutable/assets/KaTeX_Size1-Regular.c943cc98.woff",
+  a + "/_app/immutable/assets/KaTeX_Size1-Regular.95b6d2f1.ttf",
+  a + "/_app/immutable/assets/KaTeX_Size2-Regular.d04c5421.woff2",
+  a + "/_app/immutable/assets/KaTeX_Size2-Regular.2014c523.woff",
+  a + "/_app/immutable/assets/KaTeX_Size2-Regular.a6b2099f.ttf",
+  a + "/_app/immutable/assets/KaTeX_Size3-Regular.6ab6b62e.woff",
+  a + "/_app/immutable/assets/KaTeX_Size3-Regular.500e04d5.ttf",
+  a + "/_app/immutable/assets/KaTeX_Size4-Regular.a4af7d41.woff2",
+  a + "/_app/immutable/assets/KaTeX_Size4-Regular.99f9c675.woff",
+  a + "/_app/immutable/assets/KaTeX_Size4-Regular.c647367d.ttf",
+  a + "/_app/immutable/assets/KaTeX_Typewriter-Regular.71d517d6.woff2",
+  a + "/_app/immutable/assets/KaTeX_Typewriter-Regular.e14fed02.woff",
+  a + "/_app/immutable/assets/KaTeX_Typewriter-Regular.f01f3e87.ttf",
+  a + "/_app/immutable/chunks/singletons.4ec2e51a.js",
+  a + "/_app/immutable/chunks/timeFormat.096cd535.js",
+  a + "/_app/immutable/chunks/tippy.8fe2a34b.js",
+  a + "/_app/immutable/assets/tippy.7df0e90e.css",
+  a + "/_app/immutable/entry/start.77fb45b8.js"
+], _ = [
+  a + "/example/example2.mp4",
+  a + "/favicon.ico",
+  a + "/favicon.png",
+  a + "/robots.txt",
+  a + "/~partytown/debug/partytown-atomics.js",
+  a + "/~partytown/debug/partytown-media.js",
+  a + "/~partytown/debug/partytown-sandbox-sw.js",
+  a + "/~partytown/debug/partytown-sw.js",
+  a + "/~partytown/debug/partytown-ww-atomics.js",
+  a + "/~partytown/debug/partytown-ww-sw.js",
+  a + "/~partytown/debug/partytown.js",
+  a + "/~partytown/partytown-atomics.js",
+  a + "/~partytown/partytown-media.js",
+  a + "/~partytown/partytown-sw.js",
+  a + "/~partytown/partytown.js"
+], o = [
+  a + "/",
+  a + "/about",
+  a + "/atom.xml",
+  a + "/example",
+  a + "/feed.json",
+  a + "/hello-QWER",
+  a + "/manifest.webmanifest",
+  a + "/quick-start",
+  a + "/sitemap.xml",
+  a + "/versions.json"
+], l = "1684887692228", p = self, f = `cache${l}`, c = u.concat(_).concat(o), r = new Set(c);
+p.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(f).then((s) => s.addAll(c)).then(() => {
+      p.skipWaiting();
+    })
+  );
+});
+p.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(async (s) => {
+      for (const t of s)
+        t !== f && await caches.delete(t);
+      p.clients.claim();
+    })
+  );
+});
+async function d(e) {
+  const s = await caches.open(`offline${l}`);
+  try {
+    const t = await fetch(e);
+    return s.put(e, t.clone()), t;
+  } catch (t) {
+    const i = await s.match(e);
+    if (i)
+      return i;
+    throw t;
+  }
+}
+p.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET" || e.request.headers.has("range"))
+    return;
+  const s = new URL(e.request.url), t = s.protocol.startsWith("http"), i = s.hostname === self.location.hostname && s.port !== self.location.port, m = s.host === self.location.host && r.has(s.pathname), b = e.request.cache === "only-if-cached" && !m;
+  t && !i && !b && e.respondWith(
+    (async () => m && await caches.match(e.request) || d(e.request))()
+  );
+});
